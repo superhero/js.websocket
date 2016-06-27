83b06faf44567d01ae5baf447ec07e058710a887
Erik Landvall <erik@landvall.se>
2016-06-27 14:54:27 +0200

    Returning a "promise" like object

--- socket.client.js
+++ socket.client.js
@@ -1,93 +1,114 @@
-function Socket(options, callback)
+define(function()
 {
-  var
-  pid,
-  observers = {},
-  self      = this,
-  config    =
+  function Socket(options, callback)
   {
-    protocol: options.protocol || 'ws',
-    host    : options.host     || '127.0.0.1',
-    debug   : options.debug    || false
-  },
-  debug = function(context, data)
-  {
-    config.debug
-    && window.console
-    && window.console.log
-    && window.console.log(context, data);
-  },
-  socket = new WebSocket(config.protocol + '://' + config.host);
-
-  socket.onopen = function(event)
-  {
-    debug('connection open', event);
-    self.emit('connected');
-    pid = setInterval(function(){self.emit('ping')}, 25000);
-    callback(self);
-  };
-
-  socket.onclose = function(event)
-  {
-    debug('conection closed', event);
-    clearInterval(pid);
-  };
-
-  socket.onmessage = function(event)
-  {
-    debug('recived message', event);
-
     var
-    dto = JSON.parse(event.data)
-    obs = observers[dto.event];
-
-    for(var i = 0, l = obs ? obs.length : 0; i < l; i++)
-      obs[i].call(self, dto.data);
-  };
-
-  this.emit = function(event, data)
-  {
-    var dto =
+    pid,
+    observers = {},
+    self      = this,
+    config    =
     {
-      event : event,
-      data  : data
+      protocol: options.protocol || 'ws',
+      host    : options.host     || '127.0.0.1',
+      debug   : options.debug    || false
+    },
+    debug = function(context, data)
+    {
+      config.debug
+      && window.console
+      && window.console.log
+      && window.console.log(context, data);
+    },
+    socket = new WebSocket(config.protocol + '://' + config.host);
+
+    socket.onopen = function(event)
+    {
+      debug('connection open', event);
+      self.emit('connected');
+      pid = setInterval(function(){self.emit('ping')}, 25000);
+      callback();
     };
-    socket.send(JSON.stringify(dto));
-    debug('emitted', dto);
-    return this;
-  };
 
-  this.on = function(event, observer)
-  {
-    if(!observers[event])
-      observers[event] = [];
+    socket.onclose = function(event)
+    {
+      debug('conection closed', event);
+      clearInterval(pid);
+    };
 
-    observers[event].push(observer);
-    debug('added observer', observer);
-    return this;
-  };
+    socket.onmessage = function(event)
+    {
+      debug('recived message', event);
 
-  this.removeListener = function(event, listener)
-  {
-    if(!observers[event])
+      var
+      dto = JSON.parse(event.data)
+      obs = observers[dto.event];
+
+      for(var i = 0, l = obs ? obs.length : 0; i < l; i++)
+        obs[i].call(self, dto.data);
+    };
+
+    this.emit = function(event, data)
+    {
+      var dto =
+      {
+        event : event,
+        data  : data
+      };
+      socket.send(JSON.stringify(dto));
+      debug('emitted', dto);
       return this;
+    };
 
-    var index = observers[event].indexOf(listener);
-    ~index && observers[event].splice(index, 1);
-    debug('removed observer', listener);
-    return this;
-  };
+    this.on = function(event, observer)
+    {
+      if(!observers[event])
+        observers[event] = [];
 
-  this.close = function()
+      observers[event].push(observer);
+      debug('added observer', observer);
+      return this;
+    };
+
+    this.removeListener = function(event, listener)
+    {
+      if(!observers[event])
+        return this;
+
+      var index = observers[event].indexOf(listener);
+      ~index && observers[event].splice(index, 1);
+      debug('removed observer', listener);
+      return this;
+    };
+
+    this.close = function()
+    {
+      socket.close();
+    };
+  }
+
+  return function(options)
   {
-    socket.close();
-  };
-}
+    // keeping it like this sense we don't have any good polyfill for a
+    // real promise.. @todo
+    var
+    observers = [],
+    ready = false,
+    socket = new Socket(options, function()
+    {
+      ready = true;
+      while(var observer = observers.shift())
+        observer(socket);
+    }),
+    promise =
+    {
+      then: function(observer)
+      {
+        ready
+        ? observer(socket)
+        : observers.push(observer);
+      }
+    };
 
-define && define(function()
-{
-  return function(options, observer)
-  {
-    new Socket(options, observer);
+    return promise;
   };
 });
