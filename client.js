946f3acd60c25744f826f3c71c0fd7c721aa5347
Erik Landvall <erik@landvall.se>
2016-06-30 11:46:42 +0200

    -.-

--- client.js
+++ client.js
@@ -1,16 +1,13 @@
-/*
- * This is the browser code
- */
 define(function()
 {
-  function Socket(options, onConnected)
+  return function(options, onConnected)
   {
     var
     pid,
+    socket,
     observers = {},
     initQueue = [],
     connected = false,
-    self      = this,
     config    =
     {
       protocol: options.protocol || 'ws',
@@ -24,95 +21,105 @@ define(function()
       && window.console.log
       && window.console.log(context, data);
     },
-    socket = new WebSocket(config.protocol + '://' + config.host);
-
-    socket.onopen = function(event)
+    // interface
+    face =
     {
-      debug('connection open', event);
-      self.emit('connected');
-      pid = setInterval(function(){self.emit('ping')}, 25000);
-
-      // this set is used for the init queue (connection promise)
-      connected = true;
-      onConnected && onConnected();
-      var observer;
-      while(observer = initQueue.shift())
-        observer(self);
-    };
-
-    socket.onclose = function(event)
-    {
-      debug('conection closed', event);
-      connected = false;
-      clearInterval(pid);
-    };
-
-    socket.onmessage = function(event)
-    {
-      debug('recived message', event);
-
-      var
-      dto = JSON.parse(event.data)
-      obs = observers[dto.event];
-
-      for(var i = 0, l = obs ? obs.length : 0; i < l; i++)
-        obs[i].call(self, dto.data);
-    };
-
-    // This method works like a promise, a promise that the connection is
-    // established
-    this.connected = function(observer)
-    {
-      connected
-      ? observer(self)
-      : initQueue.push(observer);
-      return this;
-    };
-
-    this.emit = function(event, data)
-    {
-      var dto =
+      // This method works like a promise, a promise that the connection is
+      // established
+      connected: function(observer)
       {
-        event : event,
-        data  : data
-      };
-      setTimeout(function()
-      {
-        socket.send(JSON.stringify(dto));
-        debug('emitted', dto);
-      });
-      return this;
-    };
-
-    this.on = function(event, observer)
-    {
-      if(!observers[event])
-        observers[event] = [];
-
-      observers[event].push(observer);
-      debug('added observer', observer);
-      return this;
-    };
-
-    this.removeListener = function(event, listener)
-    {
-      if(!observers[event])
+        connected
+        ? observer(this)
+        : initQueue.push(observer);
         return this;
+      },
 
-      var index = observers[event].indexOf(listener);
-      ~index && observers[event].splice(index, 1);
-      debug('removed observer', listener);
-      return this;
+      emit: function(event, data)
+      {
+        var dto =
+        {
+          event : event,
+          data  : data
+        };
+        setTimeout(function()
+        {
+          socket.send(JSON.stringify(dto));
+          debug('emitted', dto);
+        });
+        return this;
+      },
+
+      on: function(event, observer)
+      {
+        if(!observers[event])
+          observers[event] = [];
+
+        observer instanceof Function
+        && observers[event].push(observer);
+
+        return this;
+      },
+
+      trigger: function(event, data)
+      {
+        (observers[event] || []).forEach(function(obs)
+        {
+          obs(face, data);
+        });
+      },
+
+      removeListener: function(event, listener)
+      {
+        if(!observers[event])
+          return this;
+
+        var index = observers[event].indexOf(listener);
+        ~index && observers[event].splice(index, 1);
+        debug('removed observer', listener);
+        return this;
+      },
+
+      close: function()
+      {
+        socket.close();
+      }
     };
 
-    this.close = function()
+    // Jump the event queue
+    setTimeout(function()
     {
-      socket.close();
-    };
-  }
+      socket = new WebSocket(config.protocol + '://' + config.host);
 
-  return function(options, onConnected)
-  {
-    return new Socket(options, onConnected);
+      socket.onopen = function(event)
+      {
+        debug('socket open', event);
+        face.emit('connected');
+        pid = setInterval(function(){face.emit('ping')}, 25000);
+
+        // this set is used for the init queue (connection promise)
+        connected = true;
+        onConnected && onConnected();
+        var observer;
+        while(observer = initQueue.shift())
+          observer(face);
+      };
+
+      socket.onclose = function(event)
+      {
+        debug('socket closed', event);
+        connected = false;
+        clearInterval(pid);
+      };
+
+      socket.onmessage = function(event)
+      {
+        debug('socket recived message', event);
+
+        var dto = JSON.parse(event.data);
+        face.trigger(dto.event, dto.data);
+      };
+    });
+
+    return face;
   };
 });
